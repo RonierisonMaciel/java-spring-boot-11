1b8fed4da039097e1e0d234a07a63515f041690c
Ronierison Maciel <ronierison.maciel@gmail.com>
2021-02-20 19:37:32 -0300

    Project Created

---
+++ src/test/java/edu/unirios/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unirios.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
